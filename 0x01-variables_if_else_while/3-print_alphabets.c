bc2544998492cc5105bbf1e0bec01b2d487d6966
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-03 12:45:53 -0700

    lowerCase and upperCase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowerCase = 'a';
+	char upperCase = 'A';
+
+	while (lowerCase <= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase++;
+	}
+
+	while (upperCase <= 'Z')
+	{
+		putchar(upperCase);
+		upperCase++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
