2bf33c589e8478705617e1ad99c96da4d4a400e7
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-03 15:10:49 -0700

    combination 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tens;
+	int units;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (units = (tens + 1); units <= '9'; units++)
+		{
+			putchar(tens);
+			putchar(units);
+
+			if (tens != '8' || units != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
