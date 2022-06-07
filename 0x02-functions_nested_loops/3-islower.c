6c1d1191b478f8dfb4fda0d891e08d17e5a3c10b
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-07 12:43:53 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ *
+ * Return: A1 if lowercase, 0 if uppercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	_putchar('\n');
+}
