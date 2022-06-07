eb1467c9b1337ddfb41908239629e119c69c408c
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-07 10:44:07 -0700

    print alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alpha followed by a new line
+ *
+ * Return: Always 0 (Success
+ */
+
+void print_alphabet_x10(void)
+{
+	char times = 0, alpha;
+
+	while (times <= 10)
+	{
+		alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+
+		_putchar('\n');
+		times++;
+	}
+
+	_putchar('\n');
+}
