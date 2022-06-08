bcabdc81885c47e2fd970d6b70ae6066321a8979
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-08 09:16:19 -0700

    50 fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fib
+ *
+ * Return: Always 0 (Success)
+ */
+
+void main()
+{
+	int a = 1, b = 2, c, num = 50, count = 0;
+
+	printf("%d, ", a);
+	printf("%d, ", b);
+	count = 2;
+
+	while (count < num)
+	{
+		c = a + b;
+		count++;
+		printf("%d, ", c);
+		a = b;
+		b = c;
+	}
+}
