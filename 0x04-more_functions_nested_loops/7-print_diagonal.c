e9dea3857b183995abad50cd9b687dd997f5f436
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-09 15:00:30 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: no of times characte should be printed
+ */
+void print_diagonal(int n)
+{
+	int gap;
+	int slash;
+
+	if (n > 0)
+	{
+		slash = 1;
+
+		while (slash <= n)
+		{
+			 for (gap = 1; gap < slash; gap++)
+			 {
+				 _putchar(' ');
+			 }
+			_putchar('\\');
+			_putchar('\n');
+			slash++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
