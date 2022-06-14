650f600eb5f53020b7e917cb06527aece4492695
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-14 08:36:41 -0700

    atoi

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -8,10 +8,13 @@
  */
 int main(void)
 {
-	char s[10] = "My School";
+	int nb;
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
 	return (0);
 }
