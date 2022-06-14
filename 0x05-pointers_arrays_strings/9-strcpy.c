75b3d6089862d7a0fe9dc46831b4a8d55a3de09e
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-14 07:22:33 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: first int to swap
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+/**
+ * _strcpy - copies the string pointed to by 'src', including the 
+ * terminating nnull byte(\0), to the buffer pointed to by 'dest'
+ * @src: pointer to string to be copied
+ * @dest: pointer to buffer to be copied to
+ * Return: pointer to 'dest'
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i <= _strlen(src))
+	{
+		dest[i] = src[i];	
+		i++;
+	}
+
+	return (dest);
+}
