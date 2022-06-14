c2390973986120ab82b6a1c9ade0845533668356
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-14 02:39:49 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an 'int' as params and updates the value
+ * it points to to 98
+ * @n: argument
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
