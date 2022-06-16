99abeab5d5d284178aa67aaf84c499985827a578
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-06-16 06:30:10 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the order of an array
+ * @a: array
+ * @n: no of elememts in array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1; /*omit null terminator*/
+	int rev;
+
+	while (start < end)
+	{
+		rev = *(a + start);
+		*(a + start) = *(a + end);
+		*(a + end) = rev;
+		start++, end--;
+	}	
+}
