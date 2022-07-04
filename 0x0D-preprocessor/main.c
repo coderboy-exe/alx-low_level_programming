1cdf9e79dde8b8077dbbc02e34137d266b09a8a2
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-07-04 08:22:29 -0700

    refactor code

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,12 +1,14 @@
-#include "4-sum.h"
+#include "3-function_like_macro.h"
 
 #include <stdio.h>
 
 int main(void)
 {
-	int a;
+	int i;
+	int j;
 
-	a = SUM(98, 1024);
-	printf("%d\n", a);
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
 	return (0);
 }
