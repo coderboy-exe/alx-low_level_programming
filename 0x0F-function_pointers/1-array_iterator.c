8bf78462e37d4f2bcdbb3e0f768aceedd303db89
Uchechukwu Samuel Ottah <usottah@gmail.com>
2022-07-06 06:56:01 -0700

    fixed imports

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
- * array_iterator - executes a function given as aparameter
+ * array_iterator - executes a function given as a parameter
  * on each element of an array
  *
  * @array: array argument
