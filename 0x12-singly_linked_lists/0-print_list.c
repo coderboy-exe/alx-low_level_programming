ec35f5937395681819c31f0c30d9ee789f540f29
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-07-14 16:18:46 +0000

    remove spaces for betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to a node in the singly linked list
@@ -8,10 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t num;
+	size_t num = 0;
 
-	num = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
