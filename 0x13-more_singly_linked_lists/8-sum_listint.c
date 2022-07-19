59bd4411e144ed100d6d605ce58ca87cf21cc28d
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-07-19 11:44:33 +0000

    sum all nodes in a linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data of a list
+ * @head: the head node of the list
+ * Return: sum, 0 if the list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
