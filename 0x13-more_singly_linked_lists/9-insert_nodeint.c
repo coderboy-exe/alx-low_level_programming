e5f33efce798b78e41bb58000f9a001748848271
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-07-19 13:06:12 +0000

    new index handler

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,8 +14,15 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new_node;
+	unsigned int i;
 
 	temp = *head;
+
+	if (idx != 0)
+	{
+		for (i = 0; (i < idx - 2) && (temp != NULL); i++)
+			temp = temp->next;
+	}
 
 	if (temp == NULL && idx != 0)
 		return (NULL);
