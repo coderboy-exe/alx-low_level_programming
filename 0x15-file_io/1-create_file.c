31920126ecd6390a70eb9e770fe17f5f93322c71
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-07-25 13:45:12 +0000

    hotfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (0);
 
 	if (text_content == NULL)
-		text_content = "";
+		write(fd, '\0', 1);
 
 	for (i = 0; i < text_content[i]; i++)
 		;
