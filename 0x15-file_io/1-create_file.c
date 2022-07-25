9bdad0b476dbb6c1ff25c8a7d06f38139105a0dd
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-07-25 13:38:53 +0000

    function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the file to read
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, wrt;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
+	if (fd == -1)
+		return (0);
+
+	if (text_content == NULL)
+		write(fd, '\0', 0);
+
+	for (i = 0; i < text_content[i]; i++)
+		;
+	wrt = write(fd, text_content, i);
+
+	if (wrt == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,6 @@
 #include <unistd.h>
 
 ssize_t read_textfile(const char *file, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
