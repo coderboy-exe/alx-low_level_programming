ec967e5e9c82a75fb387ff60b6e7fe758dabb440
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-07-25 13:41:22 +0000

    change handler for empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (0);
 
 	if (text_content == NULL)
-		write(fd, '\0', 0);
+		text_content = "";
 
 	for (i = 0; i < text_content[i]; i++)
 		;
