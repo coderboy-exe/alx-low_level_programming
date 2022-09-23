cb4b4d5082795cf9fae974998dcbf36dc384147c
coderboy-exe <coderboy.exe@gmail.com>
2022-09-23 11:34:19 +0000

    add NULL conditionn for value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item, *new;
 	unsigned long int idx;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	idx = key_index((unsigned char *)key, ht->size);
