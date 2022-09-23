f793d541f57966762b24390d80a41698bbfc1acb
coderboy-exe <coderboy.exe@gmail.com>
2022-09-23 09:33:42 +0000

    Return key index in hash table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/*
+ * key_index - returns the index of a given key in the hash table
+ * @key: the key
+ * @size: size of the array of the hash table
+ *
+ * Return: Returns the index at which the key/value pair should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+	return (index % size);
+}
