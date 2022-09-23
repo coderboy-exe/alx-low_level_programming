b688b50dc6b3ee5b93876ab271b0f79bc7781495
coderboy-exe <coderboy.exe@gmail.com>
2022-09-23 11:33:14 +0000

    Bug fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	idx = key_index((unsigned char *)key, ht->size);
 	item = ht->array[idx];
 
-	while (item)
+	if (item)
 	{
 		if (strcmp(item->key, key) == 0)
 		{
