ad5bdbdb779e5f1eb982eae8a21586daccc323cc
Uchechukwu Ottah <coderboy.exe@gmail.com>
2023-01-03 16:16:11 +0000

    initial commit: Implemented linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers using the Linear
+ * Search Algorithm
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: the first index where the value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	if (!(value) || array == NULL)
+		return (-1);
+
+	i = 0;
+	while (i < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			return (i);
+			break;
+		}
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
