4657fe7d3de77a16b83cf4a2c031277a15dab10a
Uchechukwu Ottah <coderboy.exe@gmail.com>
2023-01-03 17:08:44 +0000

    Binary search algo (buggy)

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers using the Linear
+ * Search Algorithm
+ *
+ * @array: a pointer to the first element of the array (Sorted in ascending order)
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: the first index where the value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+	unsigned int beg, mid, end;
+
+	if (!(value) || array == NULL)
+		return (-1);
+
+	i = 0;
+	beg = 0;
+	mid = 0;
+	end = size - 1;
+	while (i <= end)
+	{
+		printf("Searching in array: ");
+		for (i = 0; i <= end; i++)
+		{
+				if (i != end)
+				printf("%d, ", array[i]);
+			else
+				printf("%d\n", array[i]);
+		}
+
+		mid = (beg + end) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+		{
+			end = mid - 1;
+		}
+		else
+			beg = mid + 1;
+	}
+	return (-1);
+}
